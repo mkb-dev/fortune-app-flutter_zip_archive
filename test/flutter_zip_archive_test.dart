00c513bd40d690ab9e10aaf9200ff36cc2b272af
yuki <y.style.site@gmail.com>
2023-03-17 16:32:13 +0900

    pub update

--- test/flutter_zip_archive_test.dart
+++ test/flutter_zip_archive_test.dart
@@ -1,10 +1,10 @@
-import 'package:flutter_zip_archive/flutter_zip_archive.dart';
+// import 'package:flutter_zip_archive/flutter_zip_archive.dart';
 
-void main() {
-  testW('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
-}
+// void main() {
+//   testW('adds one to input values', () {
+//     final calculator = Calculator();
+//     expect(calculator.addOne(2), 3);
+//     expect(calculator.addOne(-7), -6);
+//     expect(calculator.addOne(0), 1);
+//   });
+// }
