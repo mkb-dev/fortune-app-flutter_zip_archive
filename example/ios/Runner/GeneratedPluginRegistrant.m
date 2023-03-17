c3e980cac2e21b95df374960bcef49be5bce1119
yuki <y.style.site@gmail.com>
2023-03-17 14:02:49 +0900

    update sdk

--- example/ios/Runner/GeneratedPluginRegistrant.m
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -2,10 +2,27 @@
 //  Generated file. Do not edit.
 //
 
+// clang-format off
+
 #import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_zip_archive/FlutterZipArchivePlugin.h>)
 #import <flutter_zip_archive/FlutterZipArchivePlugin.h>
+#else
+@import flutter_zip_archive;
+#endif
+
+#if __has_include(<image_picker/ImagePickerPlugin.h>)
 #import <image_picker/ImagePickerPlugin.h>
+#else
+@import image_picker;
+#endif
+
+#if __has_include(<path_provider/PathProviderPlugin.h>)
 #import <path_provider/PathProviderPlugin.h>
+#else
+@import path_provider;
+#endif
 
 @implementation GeneratedPluginRegistrant
 
